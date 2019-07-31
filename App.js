66419eb9f83d9f6bacda8cb16b039e3d1bf8ade9
Juliovaz <julio.oliveiravaz0@gmail.com>
2019-07-31 17:31:09 -0300

    finalizado navigation inicial

--- App.js
+++ App.js
@@ -1,133 +1,221 @@
-import React, {Component} from 'react';
-import {
-    View,
-    Text,
-    StyleSheet,
-    Button
-} from 'react-native';
+import React, { Component } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 // navigator
 import {
-    createSwitchNavigator,
-    createAppContainer,
-    createDrawerNavigator,
-    createBottomTabNavigator,
-    createStackNavigator,
+  createSwitchNavigator,
+  createAppContainer,
+  createDrawerNavigator,
+  createBottomTabNavigator,
+  createStackNavigator,
 } from 'react-navigation';
 
 import Icon from 'react-native-vector-icons/FontAwesome';
 
-
-const App = () => <AppContainer/> ;
+const App = () => <AppContainer />;
 
 export default App;
 
-class WelcomeScreen extends Component{
-    render(){
-        return(
-            <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-                <Button title='Login' onPress={() => this.props.navigation.navigate('Dashboard')} />
-                <Button title='Sign up' onPress={() => alert('button pressed')} />
-            </View>
-        );
-    }
+class WelcomeScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          title="Login"
+          onPress={() => this.props.navigation.navigate('Dashboard')}
+        />
+        <Button title="Sign up" onPress={() => alert('button pressed')} />
+      </View>
+    );
+  }
 }
 
-class DashboardScreen extends Component{
-    render(){
-        return(
-            <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>DashboardScreen</Text>
-            </View>
-        );
-    }
+class DashboardScreen extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>DashboardScreen</Text>
+      </View>
+    );
+  }
 }
 
-class Feed extends Component{
-    render(){
-        return(
-            <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>Feed</Text>
-            </View>
-        )
-    }
+class Feed extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Button
+          title="Go to Detail Screen"
+          onPress={() => this.props.navigation.navigate('Detail')}
+        />
+      </View>
+    );
+  }
 }
 
-class Profile extends Component{
-    render(){
-        return(
-            <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>Profile</Text>
-            </View>
-        )
-    }
+class Profile extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Profile</Text>
+      </View>
+    );
+  }
 }
 
-class Settings extends Component{
-    render(){
-        return(
-            <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-                <Text>Settings</Text>
-            </View>
-        )
-    }
+class Settings extends Component {
+  render() {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Settings</Text>
+      </View>
+    );
+  }
 }
 
-
-const DashboardTabNavigator = createBottomTabNavigator({
-    Feed,
-    Profile,
-    Settings
-}, {
-    navigationOptions: ({navigation}) => {
-        const {routeName} = navigation.state.routes[navigation.state.index];
-        return {
-            headerTitle: routeName
-        }
-    }
-}
+const Detail = props => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Detail</Text>
+  </View>
 );
 
-const DashboardStackNavigator = createStackNavigator({
-    DashboardTabNavigator: DashboardTabNavigator
-}, {
-    defaultNavigationOptions: ({navigation}) => {
+const SettingsStack = createStackNavigator({
+  Feed: {
+    screen: Settings,
+    navigationOptions: ({ navigation }) => {
+      return {
+        title: 'Settings',
+        headerLeft: (
+          <Icon
+            style={{
+              paddingLeft: 10,
+            }}
+            onPress={() => navigation.openDrawer()}
+            name="bars"
+            size={30}
+            color="#000"
+          />
+        ),
+      };
+    },
+  },
+});
+
+const ProfileStack = createStackNavigator({
+  Feed: {
+    screen: Profile,
+    navigationOptions: ({ navigation }) => {
+      return {
+        title: 'Profile',
+        headerLeft: (
+          <Icon
+            style={{
+              paddingLeft: 10,
+            }}
+            onPress={() => navigation.openDrawer()}
+            name="bars"
+            size={30}
+            color="#000"
+          />
+        ),
+      };
+    },
+  },
+});
+
+const FeedStack = createStackNavigator(
+  {
+    Feed: {
+      screen: Feed,
+      navigationOptions: ({ navigation }) => {
         return {
-            headerLeft: (
-                <Icon
-                style={{
-                    paddingLeft: 10,
-                }}
-                onPress={() => navigation.openDrawer()}
-                name="bars"
-                size={30}
-                color="#000"/>
-            )
-        }
-    }
-})
+          title: 'Feed',
+          headerLeft: (
+            <Icon
+              style={{
+                paddingLeft: 10,
+              }}
+              onPress={() => navigation.openDrawer()}
+              name="bars"
+              size={30}
+              color="#000"
+            />
+          ),
+        };
+      },
+    },
+    Detail: {
+      screen: Detail,
+    },
+  },
+  {
+    defaultNavigationOptions: {
+      gesturesEnabled: false, // desabilita handlers exceto o deslizar para o right do drawer
+    },
+  }
+);
+
+const DashboardTabNavigator = createBottomTabNavigator(
+  {
+    FeedStack,
+    ProfileStack,
+    SettingsStack,
+  },
+  {
+    navigationOptions: ({ navigation }) => {
+      const { routeName } = navigation.state.routes[navigation.state.index];
+      return {
+        header: null,
+        headerTitle: routeName,
+      };
+    },
+  }
+);
+
+const DashboardStackNavigator = createStackNavigator(
+  {
+    DashboardTabNavigator: DashboardTabNavigator,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => {
+      return {
+        headerLeft: (
+          <Icon
+            style={{
+              paddingLeft: 10,
+            }}
+            onPress={() => navigation.openDrawer()}
+            name="bars"
+            size={30}
+            color="#000"
+          />
+        ),
+      };
+    },
+  }
+);
 
 const AppDrawerNavigator = createDrawerNavigator({
-    Dashboard: {
-        screen: DashboardStackNavigator
-    }
-})
+  Dashboard: {
+    screen: DashboardStackNavigator,
+  },
+});
 
 const appSwitchNavigator = createSwitchNavigator({
-    Welcome: {
-        screen: WelcomeScreen
-    },
-    Dashboard: {
-        screen: AppDrawerNavigator
-    }
+  Welcome: {
+    screen: WelcomeScreen,
+  },
+  Dashboard: {
+    screen: AppDrawerNavigator,
+  },
 });
 
 const AppContainer = createAppContainer(appSwitchNavigator);
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center'
-    }
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
 });
